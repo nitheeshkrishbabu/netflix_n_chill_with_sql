ed7aadaa2e1454118f9e4f316b97d278121f45f4
Nitheesh Krishna <nitheeshkrishbabu@gmail.com>
2025-04-26 23:35:00 +0530

    Update Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -1,20 +1,20 @@
 -- SCHEMAS of Netflix
 
 DROP TABLE IF EXISTS netflix;
-CREATE TABLE netflix
+CREATE TABLE netflix_data
 (
-	show_id	VARCHAR(5),
+	show_id	VARCHAR(8),
 	type    VARCHAR(10),
-	title	VARCHAR(250),
-	director VARCHAR(550),
-	casts	VARCHAR(1050),
-	country	VARCHAR(550),
-	date_added	VARCHAR(55),
+	title	VARCHAR(150),
+	director VARCHAR(210),
+	casts	VARCHAR(1000),
+	country	VARCHAR(150),
+	date_added	VARCHAR(50),
 	release_year	INT,
-	rating	VARCHAR(15),
+	rating	VARCHAR(10),
 	duration	VARCHAR(15),
-	listed_in	VARCHAR(250),
-	description VARCHAR(550)
+	listed_in	VARCHAR(100),
+	description VARCHAR(250)
 );
 
-SELECT * FROM netflix;
+SELECT * FROM netflix_data;
